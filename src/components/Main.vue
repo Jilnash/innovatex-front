ac28315cfb5042139e5beff76470b73f81f091ae
= <=>
2023-09-24 12:24:25 +0600

    2nd message

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,7 @@
       <router-link to="adviser">Enviromental<br> Recomedations</router-link>
       <router-link to="suggestion">Place <br>adviser</router-link>
     </div>
-    <div class="widgets">Widgets</div>
+    <div class="widgets"><b>Currency:</b> RUB {{ RUB.toFixed(2) }}, USD {{ USD.toFixed(2) }}, EUR {{ EUR.toFixed(2) }}</div>
   </div>
 </div>
 </template>
@@ -16,7 +16,28 @@ export default {
   name: "Main",
   data() {
     return {
+      RUB: null,
+      EUR: null,
+      USD: null,
     }
+  },
+  created() {
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://api.currencyapi.com/v3/latest?base_currency=EUR&currencies=KZT&apikey=cur_live_kTACYBSqXf7jCFgqFk1H4RKDj8jibaSLJC8v0zan", requestOptions)
+        .then(response => response.json())
+        .then(result => this.EUR = result.data.KZT.value)
+
+    fetch("https://api.currencyapi.com/v3/latest?base_currency=USD&currencies=KZT&apikey=cur_live_kTACYBSqXf7jCFgqFk1H4RKDj8jibaSLJC8v0zan", requestOptions)
+        .then(response => response.json())
+        .then(result => this.USD = result.data.KZT.value)
+
+    fetch("https://api.currencyapi.com/v3/latest?base_currency=RUB&currencies=KZT&apikey=cur_live_kTACYBSqXf7jCFgqFk1H4RKDj8jibaSLJC8v0zan", requestOptions)
+        .then(response => response.json())
+        .then(result => this.RUB = result.data.KZT.value)
   },
   methods: {
   }
